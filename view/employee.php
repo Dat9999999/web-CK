2500a57194f38f89e99e63399a65a21a5f3c395b
Dat9999999 <huynhtandat184@gmail.com>
2024-04-20 13:20:03 +0700

    employee

--- view/employee.php
+++ view/employee.php
@@ -1,4 +1,15 @@
 <link rel="stylesheet" href="./view/asset/css/Nhanvien.css" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+    <link rel="preconnect" href="https://fonts.gstatic.com" />
+    <link
+      rel="stylesheet"
+      href="./asset/font/fontawesome-free-6.5.1-web/css/all.min.css"
+    />
+    <script
+      src="https://kit.fontawesome.com/a076d05399.js"
+      crossorigin="anonymous"
+    ></script>
 <div class="staff">
       <h2>Danh sách nhân viên</h2>
       <div class="staff-header">
@@ -11,12 +22,10 @@
         />
         <button class="staff-button">Tìm Kiếm</button>
         <button class="staff-btnadd">Thêm Nhân Viên</button>
-        <form id="addstaffForm" class="add-staff-form">
+        <form action ="employee.php" method="post"  id="addstaffForm" class="add-staff-form">
           <label for="">Thêm Nhân Viên</label><br />
           <input type="text" id="Name" placeholder="Name" />
-          <input type="text" id="Country" placeholder="Country" />
-          <input type="text" id="Position" placeholder="Position" />
-          <input type="text" id="Age" placeholder="Age" />
+          <input type="email" id="email" placeholder="email" />
           <button type="submit" onclick="addStaff()">Add staff</button>
         </form>
 
@@ -30,7 +39,9 @@
               <th>Ngày bắt đầu làm</th>
             </tr>
           </thead>
-          <tbody class="staff-list-body"></tbody>
+          <tbody class="staff-list-body">
+             
+          </tbody>
         </table>
       </div>
     </div>
